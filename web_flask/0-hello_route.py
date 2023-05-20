dd7fcc9ce726d8d070c5eb26ba3495f70482d55a
Ubuntu <rikohssn@gmail.com>
2023-05-20 20:34:02 +0000

    WSGI

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""a flask script to display “Hello HBNB!”"""
+"""WSGI flask script to display “Hello HBNB!”"""
 from flask import Flask
 
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/airbnb-onepage/')
 def index():
     """a method to return hello HBNB"""
     return "Hello HBNB!"
